13e90388569b916fb936626c614f3afbb66f0834
Alex <alex.hernandez24kb@gmail.com>
2022-02-22 15:07:30 -0600

    Componente CounterApp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+//import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 
 import './index.css';
+
 
 
 
 const divRoot = document.querySelector('#app');
 
 
-ReactDom.render( <PrimeraApp saludo = "Hola Mundo"/> , divRoot );
+ReactDom.render( <CounterApp value = {10}/> , divRoot );
 
