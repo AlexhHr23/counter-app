bfa4c8e72f78d40e9af979e5e3e792d7530063c7
Alex <alex.hernandez24kb@gmail.com>
2022-02-22 00:50:23 -0600

    Nuestro primer Componente

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import PrimeraApp from './PrimeraApp';
 
-const saludo = <h1>Hola Mundo</h1>;
+import './index.css';
+
 
 
 const divRoot = document.querySelector('#app');
 
 
-ReactDom.render( saludo, divRoot );
+ReactDom.render( <PrimeraApp /> , divRoot );
 
